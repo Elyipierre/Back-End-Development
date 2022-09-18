468288820d26a7ca508e763644caf55b597af781
Elyi Pierre <elyipierre@gmail.com>
2022-09-18 01:15:56 -0400

    promineo Tech Week 2

---
+++ Promineo Tech/src/Class2/conditionals.java
@@ -0,0 +1,67 @@
+package Class2;
+
+public class conditionals {
+
+	public static void main(String[] args) {
+		String name = "Natasha Pierre";
+		
+		if (name == "Natasha") {
+			System.out.println("Hello Baby Pierre!");
+			} else { 
+				System.out.println("You are not Baby Pierre!");
+			}
+		int age = 5;
+		
+		if (age >=16 ) {
+			System.out.println("You can get your license!");
+		} else {
+			System.out.println("Please wait " + (16 - age) + " year(s) before you can get your license");
+		}
+		
+		double costOfMilk = 2;
+		
+		if (costOfMilk <= 2) {
+			System.out.println("Buying 2");
+			
+		}else if (costOfMilk <= 3) {
+			System.out.println("Buying 3");
+		} else {	
+			System.out.println("Not buying any milk.");
+		}
+		
+		//switch statement
+		
+		char grade = 'A';
+		
+		switch (grade) {
+		    case 'A':
+		    	System.out.println("90%");
+		    	break;
+		    case 'B':
+		    	System.out.println("80%");
+		    	break;
+		    case 'C':
+		    	System.out.println("70%");
+		    	break;
+		    default:
+		    	System.out.println("0%");
+		    	
+		}
+		
+		// handling multiple conditions together by nesting
+		if (5 == 5) {
+			if (4 == 4) {
+				System.out.println("5 is 5 and 4 is 4");
+			} else {
+				System.out.println("5 is 5");
+			}
+		}
+		// we can accomplish the same thing by doing the following: Always flatten with logical and operator to 
+		//avoid diverge statements
+		if (5 == 5 && 4 == 4) {
+			System.out.println("yes");
+		}	
+	}
+
+}
+
---
+++ Promineo Tech/src/Class2/userInput.java
@@ -0,0 +1,45 @@
+package Class2;
+
+import java.util.Scanner;
+
+public class userInput {
+
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+        
+        System.out.println("Welcome, " + name + "!");
+        
+        int selection = 0;
+        double total = 0;
+        while (selection != 4) {
+            System.out.println("Select an option: ");
+            System.out.println("1) Add candybar to shopping cart: $1.99");
+            System.out.println("2) Add cheese to shopping cart: $6.95");
+            System.out.println("3) Add soccer ball to shopping cart: $12.99");
+            System.out.println("4) Checkout");
+            
+            selection = sc.nextInt();
+            
+            switch (selection) {
+            case 1:
+            	total += 1.99;
+            	break;
+            case 2:
+            	total += 6.95;
+            	break;
+            case 3:
+            	total =+ 12.99;
+            	break;
+            case 4:
+            	break;
+            default:
+            	System.out.println("Please pick a valid option.");
+        }
+        } //build small menu driven application    
+        System.out.println(name + ", Your current total is $" + total);
+	}
+
+}
