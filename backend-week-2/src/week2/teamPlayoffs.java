c62686caf93854e5b61ffc64f79809d9e54ec781
Elyi Pierre <elyipierre@gmail.com>
2022-09-21 16:35:41 -0400

    Team Playoffs App

--- backend-week-2/src/week2/teamPlayoffs.java
+++ backend-week-2/src/week2/teamPlayoffs.java
@@ -1,10 +1,23 @@
 package week2;
 
+import java.util.Scanner;
+
 public class teamPlayoffs {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Will my team make the playoffs?
+		Scanner team = new Scanner(System.in);
+		int wins = team.nextInt();
+		int losses = team.nextInt();
+		boolean isFall = team.nextBoolean();
+		
+		if(wins > losses && isFall == true) {
+			System.out.println("It's looking good for your team to make the playoffs.");
+		} else if( wins > losses && isFall == false) {
+			System.out.println("Your favorite team has promise, but it's too early to tell.");
+		}else {
+			System.out.println("It's not looking so good.");
+		}
 	}
 
 }
