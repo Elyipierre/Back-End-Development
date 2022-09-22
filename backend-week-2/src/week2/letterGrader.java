31d5a24fad95f55624e8996b49ae72ddb82ae12b
Elyi Pierre <elyipierre@gmail.com>
2022-09-21 23:52:22 -0400

    Letter Grader

---
+++ backend-week-2/src/week2/letterGrader.java
@@ -0,0 +1,24 @@
+package week2;
+
+import java.util.Scanner;
+
+public class letterGrader {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int numericGrade = in.nextInt();
+		
+		// write your if/else if statement here and check your conditions using the variable numericGrade
+		if (numericGrade >= 90) {
+		    System.out.println("A");
+		} else if (numericGrade >= 80 && numericGrade < 90) {
+		    System.out.println("B");
+		} else if (numericGrade >= 70 && numericGrade < 80) {
+		    System.out.println("C");
+		} else if (numericGrade >= 60 && numericGrade < 70) {
+		    System.out.println("D");
+		} else {
+		    System.out.println("F");
+		}
+	}
+}	
