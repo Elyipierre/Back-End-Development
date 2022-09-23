534267d17e9ba1c293be2da24673126376a65ea6
Elyi Pierre <elyipierre@gmail.com>
2022-09-23 08:49:55 -0400

    Added statement

--- backend-week-2/src/week2/letterGrader.java
+++ backend-week-2/src/week2/letterGrader.java
@@ -10,15 +10,15 @@ public class letterGrader {
 		
 		// write your if/else if statement here and check your conditions using the variable numericGrade
 		if (numericGrade >= 90) {
-		    System.out.println("A");
+		    System.out.println("You scored a(n) " + numericGrade + " on the assignment and reieved a passing grade of A");
 		} else if (numericGrade >= 80 && numericGrade < 90) {
-		    System.out.println("B");
+		    System.out.println("You scored a(n) " + numericGrade + " on the assignment and reieved a passing grade of B");
 		} else if (numericGrade >= 70 && numericGrade < 80) {
-		    System.out.println("C");
+		    System.out.println("You scored a(n) " + numericGrade + " on the assignment and reieved a passing grade of D");
 		} else if (numericGrade >= 60 && numericGrade < 70) {
-		    System.out.println("D");
+		    System.out.println("You scored a(n) " + numericGrade + " on the assignment and reieved a passing grade of A");
 		} else {
-		    System.out.println("F");
+		    System.out.println("You scored a(n) " + numericGrade + " on the assignment and reieved a failing grade of F");
 		}
 	}
 }	
